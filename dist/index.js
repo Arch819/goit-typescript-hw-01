6995080f0cfc293b5f27a30d4f7e0a69eba74dfd
Viacheslav <gabon.qwa@gmail.com>
2023-10-26 14:11:21 +0300

    Revert "tasc - done"
    This reverts commit 3ec046e38aec1f24afe626de05069530cd6671ad.

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import { concatenation } from "./concatenation";
-const input = document.querySelector("input");
-const button = document.querySelector("button");
-if (button && input) {
-    button.addEventListener("click", () => {
-        concatenation(input.value, "hello");
-    });
-}
-//# sourceMappingURL=index.js.map
